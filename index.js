7aac13f6358bc496c3a83fa8f49b2d3840ec63c1
thanigai1709 <babutr59@gmail.com>
2023-02-28 09:35:28 +0530

    crawl tyarget rpute added cors added

--- index.js
+++ index.js
@@ -10,12 +10,13 @@ const app = express();
 
 app.use(
 	cors({
-		origin: "*",
+		origin: ["https://pirate-crawler-fd1blmu0q-thanigai-excrin.vercel.app/", "http://localhost:3000/"],
 	})
 );
 
 app.use(bodyParser.json());
-app.get("/", async (req, res) => {
+
+app.get("/fetch-page", async (req, res) => {
 	try {
 		if (req.method !== "GET") {
 			res.status(405).send({ message: "Only GET requests allowed" });
@@ -42,6 +43,75 @@ app.get("/", async (req, res) => {
 		const html = await page.content();
 		await browser.close();
 		res.status(200).send({ status: true, page: html });
+	} catch (error) {
+		res.status(500).send({ status: false, err: error.message });
+	}
+});
+
+app.post("/crawl-targets", async (req, res) => {
+	try {
+		const { crawler } = req.body;
+		if (!crawler || crawler.length < 1) {
+			return res.status(400).send({ message: "Invalid request format" });
+		}
+
+		const browser = await puppeteer.launch({
+			args: chrome.args,
+			executablePath: "/usr/bin/chromium-browser",
+			headless: true,
+		});
+
+		const page = await browser.newPage();
+
+		const selectors = crawler.crawlTargets;
+		await page.goto(String(crawler.targetUrl));
+
+		const data = await Promise.all(
+			selectors.map(async (selector) => {
+				let innerData = await page.$eval(selector.DOMPath, (el) => {
+					return getElementData(el);
+					function getElementData(el) {
+						let data = {};
+						data["tagName"] = el.tagName;
+						data["classList"] = el.classList.toString();
+						if (el.children.length === 0 && el.innerText != "") {
+							data.innerText = el.innerText;
+						}
+						if (el.tagName === "IMG") {
+							data["src"] = el.getAttribute("src");
+						}
+						if (el.tagName === "VIDEO") {
+							data["src"] = el.getAttribute("src");
+						}
+						if (el.tagName === "IFRAME") {
+							data["src"] = el.getAttribute("src");
+						}
+						if (el.tagName === "SVG") {
+							data["svg"] = el.innerHTML;
+						}
+
+						if (el.tagName === "A") {
+							data["href"] = el.getAttribute("href");
+						}
+
+						if (el.childNodes.length > 0) {
+							data["childNodesRAW"] = el.innerHTML;
+							data["childNodes"] = [];
+							for (let i = 0; i < el.childNodes.length; i++) {
+								if (el.childNodes[i].nodeType === 1) {
+									data["childNodes"].push(getElementData(el.childNodes[i]));
+								}
+							}
+						}
+
+						return data;
+					}
+				});
+				return { [selector.keyName]: innerData };
+			})
+		);
+		await browser.close();
+		res.status(200).send({ status: true, data });
 	} catch (error) {
 		res.status(500).send({ status: false, err: error.message });
 	}
